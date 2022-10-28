46f132ca08317fa15992c5c9dcfeb77f918b450e
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-10-28 21:05:38 +0800

    ARE YOU HAPPY NOW?

--- src/pages/api/vents.ts
+++ src/pages/api/vents.ts
@@ -34,7 +34,7 @@ export default async function handler(
         data: {
           title,
           paragraph,
-          password,
+          password: password ? password : "",
         },
       });
 
