dc4ed0b4d3d246dd4cc1c5e9e99db337880e9efd
Fizzify <fizzyplayzoriginal5@gmail.com>
2022-10-29 09:15:35 +0800

    Add progress bar.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Website from "../layouts/Website";
+import NextNProgress from "nextjs-progressbar";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <Website>
+      <NextNProgress
+        color="#000"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={4}
+        showOnShallow={true}
+        options={{
+          showSpinner: false,
+        }}
+      />
       <Component {...pageProps} />
     </Website>
   );
